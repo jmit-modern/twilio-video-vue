87a3d411e72533d4482b71448831144792d72232
jmit-modern <jmit.agency@gmail.com>
2020-06-09 03:04:03 +0800

    https

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,12 @@ module.exports = {
     chainWebpack: config => {
       // remove vue-cli-service's progress output
       config.plugins.delete('progress')
+    },
+    devServer: {
+      open: process.platform === 'darwin',
+      host: '0.0.0.0',
+      port: 8080, // CHANGE YOUR PORT HERE!
+      https: true,
+      hotOnly: false,
     }
   }
